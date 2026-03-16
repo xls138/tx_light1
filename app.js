e2aaeffea5b8f9b5c71349c68094d03e1bb9d5db
xiaolisheng <1522546196@qq.com>
2026-03-16 09:44:27 +0800

    add ipv4

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ const server = http.createServer((req, res) => {
   res.end();
 });
 
-server.listen(3000, () => {
+server.listen(3000, '0.0.0.0', () => {
   console.log('Server started on port 3000');
 });
